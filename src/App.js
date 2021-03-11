5b5cc89ed67dc18703453f448c76b89122ccfe49
Desarrollador-Orjuela <desarrolladororjuela@gmail.com>
2021-03-10 23:12:24 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import { Fragment } from 'react';
+
+function App() {
+  return (
+    <div className="container mt-5 center">
+      <h1>Tareas</h1>
+      <hr></hr>
+    </div>
+  );
+}
+
+export default App;
